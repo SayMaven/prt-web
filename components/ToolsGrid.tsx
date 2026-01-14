2802af5508c668ad57d308640d6278f8888dcdd4
Hackertrap <54194385+Hackertrap@users.noreply.github.com>
2026-01-15 01:38:30 +0700

    improve the visual appearance of page tools for mobile devices

--- components/ToolsGrid.tsx
+++ components/ToolsGrid.tsx
@@ -41,7 +41,7 @@ export default function ToolsGrid() {
 
       {/* --- GRID TOOLS --- */}
       {visibleTools.length > 0 ? (
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+        <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
           {visibleTools.map((tool) => (
             <Link
               href={tool.link}
